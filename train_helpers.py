a1a1a7311fd8e5e9d00d6e80aeb29c1784cb3353
dnguyencodez <dylan.nguyen39@gmail.com>
2024-07-28 22:05:45 -0500

    l

--- train_helpers.py
+++ train_helpers.py
@@ -67,6 +67,8 @@ def get_expert_data_with_IL(env, grp: GlobalRoutePlanner, il_model: IL_Model):
         local_planner.set_global_plan(route)
         num_waypoints = len(route)
 
+        print("here")
+
         # execute local planner step to get command for the first step
         local_planner.run_step()
         next_waypoint, wp_direction = local_planner.get_incoming_waypoint_and_direction(steps=4)
@@ -82,7 +84,6 @@ def get_expert_data_with_IL(env, grp: GlobalRoutePlanner, il_model: IL_Model):
         state = (forward_camera.get_image_float(), [vehicle.get_velocity_norm(), vehicle.object.get_speed_limit(), vehicle.object.get_control().gear], command)
 
         while not done:
-            print("here")
             steer, throttle, brake = vehicle.get_autopilot_control(il_model, scalars=state[1], image=state[0], command=state[2])
             action = np.array([steer, throttle, brake])
             vehicle.apply_control(carla.VehicleControl(throttle=action[1], steer=action[0], brake=action[2]))
